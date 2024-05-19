815b0d27af651e95866eec703330bb7dd9c72afa
Abbas <abbasmnumberdar@gmail.com>
2024-05-19 03:05:14 +0300

    deployment

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from 'path'
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
@@ -9,6 +10,8 @@ import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import { app, server } from "./socket/socket.js";
 
+const __dirname = path.resolve();
+
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
@@ -16,14 +19,15 @@ const PORT = process.env.PORT || 5000;
 // Make sure to configure body parser middleware before defining routes
 app.use(express.json());
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.get("/", (req, res) => {
-    res.send("root");
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
 
